25e4b98a1e4cee8d70eb51c70026c4d9b2f86f01
diegostrg <munoz.juand@javeriana.edu.co>
2026-02-10 11:23:58 -0500

    Añado los cuadernos de los ejercicios del taller

---
+++ Prueba Python Primera Clase/main.py
@@ -0,0 +1,12 @@
+'''
+/****************************************************
+*                                                   *
+* Autor: Juan Diego Munoz Angulo                    *
+* Materia: Procesamiento de datos a gran escala     *
+* Fecha:                          *
+* Tema:                           *
+*                                                   *
+****************************************************/
+'''
+
+print("hey github")
